ffcddb9eca9f7001dfb5c9eb4834ce2bc2278077
sharmavanshika1403-bit <sharmavanshika1403@gmail.com>
2026-03-22 13:29:36 +0530

    Add files via upload

---
+++ c2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+class smart_light{
+    int brightness;
+    string brand;
+    bool ison;
+    public:
+    smart_light();
+    smart_light(string,int,bool);
+    void turn_on();
+    void turn_off();
+    void increasebrightness();
+    void decreasebrightness();
+    void displaystatus();
+    
+};
+smart_light::smart_light(){
+    brand="";
+    ison=0;
+    brightness=50;
+}
+smart_light::smart_light(string b,int bright,bool status){
+   brand="";
+    ison=0;
+    brightness=50;
+}
+void smart_light::turn_on(){
+    ison=1;
+}
+void smart_light::turn_off(){
+    ison=0;
+}
+void smart_light::increasebrightness(){
+if(brightness+10>100)
+brightness=100;
+else
+brightness+=10;
+
+}
+void smart_light::decreasebrightness(){
+    if(brightness-10<1)
+brightness=1;
+else
+brightness-=10;
+
+}
+void smart_light::displaystatus(){
+    cout<<brand;
+    cout<<brightness;
+    cout<<ison;
+}
+int main(){
+    smart_light s1;
+    s1.displaystatus();
+}
